edd9793458a6ee452cbbc2a6e10cf04ced89ac2d
David A. Viramontes <dviramontes@gmail.com>
2019-02-11 20:37:58 -0700

    refactor: main handler to response with data from db (#24)
    * refactor: main handler to response with data from db
    
    * rm: unused
    
    * refactor: return errors

--- api/main.go
+++ api/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
 	"database/sql"
+	"encoding/json"
 	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
 	_ "github.com/lib/pq"
 )
 
@@ -15,32 +16,36 @@ const (
 	DB_HOST     = "demodb.catj63cigq6x.us-east-2.rds.amazonaws.com"
 )
 
-func main() {
+type item struct {
+	Geoid10 string
+	Field   int
+}
+
+type data struct {
+	Items []item
+}
+
+type Response events.APIGatewayProxyResponse
+
+func Handler(req events.APIGatewayProxyRequest) (Response, error) {
+	headers := map[string]string{
+		"Content-Type": "application/json",
+	}
+
 	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable host=%s", DB_USER, DB_PASSWORD, DB_NAME, DB_HOST)
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
-		log.Fatal(err.Error())
+		return Response{StatusCode: 500}, err
 	}
 	defer db.Close()
 
-	log.Println("successful connection to db")
-
-	type item struct {
-		Geoid10 string
-		Field   int
-	}
-
-	type dat struct {
-		Items []item
-	}
-
-	output := dat{}
+	output := data{}
 
 	rows, err := db.Query("SELECT geoid10, b01001_001e FROM acs5.county_state_b01001_2016")
 	defer rows.Close()
 	if err != nil {
-		log.Fatal(err.Error())
+		return Response{StatusCode: 500}, err
 	}
 
 	for rows.Next() {
@@ -52,10 +57,15 @@ func main() {
 		output.Items = append(output.Items, line)
 	}
 
-	fmt.Printf("%+v\n", output)
-
-	_, err = json.Marshal(output)
+	jsonB, err := json.Marshal(output)
 	if err != nil {
-		log.Fatal(err.Error())
+		return Response{StatusCode: 500}, err
 	}
+
+	// output is json data structure
+	return Response{StatusCode: 200, Headers: headers, Body: string(jsonB)}, nil
+}
+
+func main() {
+	lambda.Start(Handler)
 }
