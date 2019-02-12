eae19ca3d3c9e15c805a5ab7b173efd101f884a0
Shift Research Lab <admin@shiftresearchlab.org>
2019-02-11 19:33:07 -0700

    Query rows (#20)
    * wip: query rows
    
    * fix: print

--- api/main.go
+++ api/main.go
@@ -1,24 +1,23 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
-
-	"fmt"
 	"database/sql"
+	"fmt"
 	_ "github.com/lib/pq"
-
 )
 
 const (
-	DB_USER = "guest"
+	DB_USER     = "guest"
 	DB_PASSWORD = "guest"
-	DB_NAME = "shift_public"
-	DB_HOST = "demodb.catj63cigq6x.us-east-2.rds.amazonaws.com"
+	DB_NAME     = "shift_public"
+	DB_HOST     = "demodb.catj63cigq6x.us-east-2.rds.amazonaws.com"
 )
 
 func main() {
-  connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable host=%s", DB_USER, DB_PASSWORD, DB_NAME, DB_HOST)
-	
+	connStr := fmt.Sprintf("user=%s password=%s dbname=%s sslmode=disable host=%s", DB_USER, DB_PASSWORD, DB_NAME, DB_HOST)
+
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
 		log.Fatal(err.Error())
@@ -27,15 +26,36 @@ func main() {
 
 	log.Println("successful connection to db")
 
-	var geo string
-	var b01001 int
-	row := db.QueryRow("SELECT geoid10, b01001_001e FROM acs5.county_state_b01001_2016 WHERE geoid10='08001'")
-	err = row.Scan(&geo, &b01001)
+	type item struct {
+		Geoid10 string
+		Field   int
+	}
+
+	type dat struct {
+		Items []item
+	}
+
+	output := dat{}
+
+	rows, err := db.Query("SELECT geoid10, b01001_001e FROM acs5.county_state_b01001_2016")
+	defer rows.Close()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
-	
-	log.Println(b01001)
-	log.Println(geo)
 
+	for rows.Next() {
+		line := item{}
+		err = rows.Scan(
+			&line.Geoid10,
+			&line.Field,
+		)
+		output.Items = append(output.Items, line)
+	}
+
+	fmt.Printf("%+v\n", output)
+
+	_, err = json.Marshal(output)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
 }
