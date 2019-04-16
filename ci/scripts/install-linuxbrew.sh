310f2474d838dbbd2af9d4151b57957b7b838b34
Patrick MacMurchy <pmacmurchy@pairin.com>
2019-04-15 19:47:36 -0600

    Add to path

--- ci/scripts/install-linuxbrew.sh
+++ ci/scripts/install-linuxbrew.sh
@@ -5,3 +5,4 @@ if test -n "$HOMEBREW_FORCE_VENDOR_RUBY" || ! command -v ruby >/dev/null; then
 	eval "`curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install-ruby`"
 fi
 exec ruby -e "`curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install`" "$@"
+export PATH="$PATH:/home/linuxbrew/.linuxbrew/bin"
